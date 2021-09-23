bd94c19660bd08204f8a3bf48b9dbb55191a0b83
Valerii Sitkevich <70900359+Sitkevich88@users.noreply.github.com>
2021-09-23 10:22:16 +0300

    Add files via upload

---
+++ main.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+function validate($x, $y, $r)
+{
+    $result = false;
+    if ($x>=0 && $y>=0){
+        if (($r/2 - $x/2)>=$y){
+            $result = true;
+        }
+    } elseif ($x>=0 && $y<0){
+        if ($x<=$r && $y>=($r/(-2))){
+            $result = true;
+        }
+    } elseif ($x<0 && $y<0){
+        if (($x**2+$y**2)<=(($r/2)**2)){
+            $result = true;
+        }
+    }
+    return $result;
+}
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $timeStart = microtime(true);
+    // collect value of input field
+    $x = $_POST['x'];
+    $y = $_POST['y'];
+    $r = $_POST['r'];
+    $res = validate($x, $y, $r) ? 'Да' : 'Нет';
+    /*$runTime = $timeNow + $_POST['now'];*/
+    $timeNow = date("H:i:s:ms");
+    $runTime = round((microtime(true) - $timeStart)*(10**6));
+    $tableRow = '<tr><td>'.$x.'</td><td>'.$y.'</td><td>'.$r.'</td><td>'.$res.'</td><td>'.$runTime.' mks</td><td>'.$timeNow.'</td></tr>';
+    echo $tableRow;
+}
